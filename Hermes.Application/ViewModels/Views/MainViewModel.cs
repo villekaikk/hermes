895390c8d328812677591a6fc0ed6841d67e8f76
Ville <ville.kaikk@gmail.com>
2025-09-15 10:44:38 +0300

    Add query string parsing to the URL

--- Hermes.Application/ViewModels/Views/MainViewModel.cs
+++ Hermes.Application/ViewModels/Views/MainViewModel.cs
@@ -1,4 +1,5 @@
 using System.Reactive;
+using Hermes.Application.Utils;
 using Hermes.Domain.Models;
 using ReactiveUI;
 
@@ -6,20 +7,49 @@ namespace Hermes.Application.ViewModels.Views;
 
 public class MainViewModel : ReactiveObject
 {
-    
-    private int _selectedIndex = 0;
+    private int _selectedIndex;
     private string _requestUrl = string.Empty;
+    private string _queryString = string.Empty;
     private RequestMethodOption _selectedMethod = null!;
     private SendRequestCallback? _sendRequestCallback;
 
     public ReactiveCommand<Unit, Unit> SendRequestCommand { get; }
+    
+    public delegate void QueryParamUpdatedEventHandler(List<QueryParam> queryParams);
+    public event  QueryParamUpdatedEventHandler QueryParamUpdated;
 
     public delegate Task SendRequestCallback(CancellationToken cancellationToken);
     
     public string RequestUrl
     {
-        get => _requestUrl;
-        set => this.RaiseAndSetIfChanged(ref _requestUrl, value);
+        get => $"{_requestUrl}{(_queryString.Length > 0 ? "?" + QueryString : string.Empty )}";
+        set
+        {
+            if (value.Contains('?') && value.Split('?').Length > 1)
+            {
+                var splits = value.Split('?');
+                _requestUrl = splits[0];
+                QueryString = splits[1];
+            }
+            else
+            {
+                this.RaiseAndSetIfChanged(ref _requestUrl, value);
+            }
+        }
+    }
+
+    public string QueryString
+    {
+        get => _queryString;
+        set
+        {
+            this.RaiseAndSetIfChanged(ref _queryString, value);
+            if (!string.IsNullOrWhiteSpace(_queryString) && _queryString.TryParseQueryParams(out var queryParams))
+            {
+                Console.WriteLine($"QueryParams: {queryParams.Select(p => $"{p.Key}={p.Value}").Aggregate((a, b) => $"{a}, {b}")}");
+                //QueryParamUpdated?.Invoke(queryParams);
+            }
+        }
     }
 
     public int SelectedIndex
@@ -50,6 +80,11 @@ public class MainViewModel : ReactiveObject
     {
         SendRequestCommand = ReactiveCommand.CreateFromTask(SendRequestAsync);
         SendRequestCommand.ThrownExceptions.Subscribe(ex => Console.WriteLine($"Exception thrown: {ex}"));
+    }
+
+    public void UpdateQueryString()
+    {
+        
     }
 
     private async Task SendRequestAsync(CancellationToken token)
